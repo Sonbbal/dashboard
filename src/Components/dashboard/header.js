5823120f86d63c49d3178c4cb4b0e99c08f381ce
Park Hyung Seo <sonbbal20@gmail.com>
2020-11-06 18:00:41 +0900

    Add Header Comopnent in Dashboard

---
+++ src/Components/dashboard/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+const Header = () => {
+    return (
+        <div>
+            <AppBar>
+                <div>로고 들어가는곳</div>
+            </AppBar>
+        </div>
+
+    )
+}
+
+export default Header;
