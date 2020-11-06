37391bebf9a270cb2d802ffb0bfc84b90881404c
Park Hyung Seo <sonbbal20@gmail.com>
2020-11-06 18:20:56 +0900

    Add dummy data in header Component

--- src/Components/dashboard/header.js
+++ src/Components/dashboard/header.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Badge from '@material-ui/core/Badge';
+import { Avatar } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 const Header = () => {
     return (
         <div>
             <AppBar>
                 <div>로고 들어가는곳</div>
+                <div>
+                    <Badge color="secondary" variant="dot" invisible={false}>
+                        <NotificationsIcon />
+                    </Badge>
+                </div>
+                <div>
+                    <Avatar>H</Avatar>
+                    <div>
+                        <p>karima Abd El-Hamid</p>
+                        <p>Admin</p>
+                    </div>
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                </div>
             </AppBar>
         </div>
 
